fa6fa98e1b64ae6290d8e068dc2509274370687a
mr <rezakazemi890@gmail.com>
2023-05-07 15:19:22 +0330

    Rabbit configuration added

---
+++ CleanArchitectureCQRS.Shared/Consumers/IMessageConsumer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CleanArchitectureCQRS.Shared.Consumers
+{
+    public interface IMessageConsumer
+    {
+        void Consume();
+    }
+}
+
